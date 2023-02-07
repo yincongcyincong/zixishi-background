ad185a11b25df5d47131a5eb147ca563173d5da0
yincong <yincong@baidu.com>
2023-02-07 20:35:18 +0800

    init

--- config/config.go
+++ config/config.go
@@ -6,4 +6,16 @@ var SDK *weapp.Client
 
 func InitConfig() {
 	SDK = weapp.NewClient("your-appid", "your-secret")
+
+	tokenGetter := func(appid, secret string) (token string, expireIn uint) {
+		expireIn = 1000
+		token = "your-custom-token"
+		return token, expireIn
+	}
+
+	sdk := weapp.NewClient(
+		"your-appid",
+		"your-secret",
+		weapp.WithAccessTokenSetter(tokenGetter),
+	)
 }
